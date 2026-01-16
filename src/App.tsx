2f320739b509d0159347ca26b18a2fad88950cf4
Hojoong Choi <crontables@gmail.com>
2026-01-16 17:55:28 +0900

    feat: Consumption 데이터 렌더링만

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
 import { Button } from './Button';
+import { MOCK_DATA } from './Constant';
 
 function App() {
   return (
     <div>
       <div>Hello World!</div>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Style No.</th>
+              <th>Supplier Item #</th>
+              <th>Fabric Name</th>
+              <th>Fabric Color</th>
+              <th>Order Qty</th>
+              <th>Unit</th>
+              <th>U/price</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {MOCK_DATA.consumptions?.length &&
+              MOCK_DATA.consumptions.map((consumption) => (
+                <tr key={consumption.id}>
+                  <td>{consumption.salesOrder.styleNumber}</td>
+                  <td>{consumption.supplierItemCode}</td>
+                  <td>{consumption.fabricName}</td>
+                  <td>{consumption.colorName}</td>
+                  <td>{consumption.orderQuantity}</td>
+                  <td>{consumption.unit}</td>
+                  <td>{consumption.unitPrice}</td>
+                  <td className="right amount">{consumption.orderAmount.toLocaleString('ko-KR')}</td>
+                </tr>
+              ))}
+
+            <tr>
+              <td colSpan={7} className="right">
+                Sub.TTL
+              </td>
+              <td className="right amount">00,000.00</td>
+            </tr>
+            <tr>
+              <td colSpan={7} className="right">
+                G.TTL
+              </td>
+              <td className="right amount">00,000.00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div>{JSON.stringify(MOCK_DATA)}</div>
+
       <Button />
     </div>
   );
